9cb3e13c24103f611624ced7912d7041d8034f6c
Narekchik <narek0305@bk.ru>
2023-09-17 14:43:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+        double rateUSD = 78.5;
+        double rateEUR = 85.5;
+        double rubles = 14560.5;
+
+        String currency = "USD";
+        System.out.println("Вы конвертируете рубли в " + currency);
+        // Если currency равно "USD", то конвертируем в доллары, иначе в евро
+        if (currency.equals("USD")) {
+            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        }
+        else {
+            System.out.println("Ваши сбережения в евро: " + rubles / rateEUR);
+        }
+    }
+}
