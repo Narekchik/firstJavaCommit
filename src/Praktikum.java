29ba5ff6749509a8a6bdf92276cfc968f5237d25
Narekchik <narek0305@bk.ru>
2023-09-27 22:53:11 +0300

    1

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -1,41 +1,103 @@
+//public class Praktikum {
+//
+//    public static void main(String[] args) {
+//        sayHello();
+//        System.out.println("Твой самый дорогой корм стоил " + maxFeedExpenseCat);
+//        System.out.println("Твой самый дорогой корм стоил " + maxFeedExpenseHamster);
+//        sayEnjoyMeal();
+//    }
+//
+//    public static double findMaxExpense(double[] expenses) {
+//        double[] feedExpensesCat = { 100.50, 236.0, 510.6, 150.20, 80.0, 172.0, 135.4 };
+//        double[] feedExpensesHamster = { 70.50, 146.0, 710.6, 250.20, 83.0, 19.0, 55.4 };
+//
+//        double maxFeedExpenseCat = 0;
+//        for (int i = 0; i < feedExpensesCat.length; i++) {
+//            if (feedExpensesCat[i] > maxFeedExpenseCat) {
+//                maxFeedExpenseCat = feedExpensesCat[i];
+//            }
+//        }
+//        return maxFeedExpenseCat;
+//
+//        double maxFeedExpenseHamster = 0;
+//        for (int i = 0; i < feedExpensesHamster.length; i++) {
+//            if (feedExpensesHamster[i] > maxFeedExpenseHamster) {
+//                maxFeedExpenseHamster = feedExpensesHamster[i];
+//            }
+//        }
+//        return maxFeedExpenseHamster;
+//    }
+//    public static void sayHello(String name) {
+//        System.out.println("Привет, Пиксель!");
+//        System.out.println("Привет, Байт!");
+//    }
+//
+//    public static int findExpensesSum(double[] expenses) {
+//        double sumFeedCat = 0;
+//        for (int i = 0; i < feedExpensesCat.length; i++) {
+//            sumFeedCat = sumFeedCat + feedExpensesCat[i];
+//        }
+//
+//        System.out.println("Всего на корм было потрачено " + sumFeedCat);
+//
+//        double sumFeedHamster = 0;
+//        for (int i = 0; i < feedExpensesHamster.length; i++) {
+//            sumFeedHamster = sumFeedHamster + feedExpensesHamster[i];
+//        }
+//        System.out.println("Всего на корм было потрачено " + sumFeedHamster);
+//    }
+//
+//    public static void sayEnjoyMeal() {
+//        System.out.println("Приятного аппетита, Пиксель!");
+//        System.out.println("Приятного аппетита, Байт!");
+//    }
+//
+//}
+//
+//
 
-import java.util.Random;
-import java.util.Scanner;
 
 public class Praktikum {
+
     public static void main(String[] args) {
-        System.out.println("Робот-помощник v1.0.");
+
+        double[] feedExpensesCat = { 100.50, 236.0, 510.6, 150.20, 80.0, 172.0, 135.4 } ;
+        double[] feedExpensesHamster = { 70.50, 146.0, 710.6, 250.20, 83.0, 19.0, 55.4 } ;
+        sayHello("Пиксель");
+        System.out.println("Твой самый дорогой корм стоил " + findMaxExpense(feedExpensesCat));
+        System.out.println("Всего на корм было потрачено " + findExpensesSum(feedExpensesCat));
+        sayEnjoyMeal("Пиксель");
+        sayHello("Байт");
+        System.out.println("Твой самый дорогой корм стоил " + findMaxExpense(feedExpensesHamster));
+        System.out.println("Всего на корм было потрачено " + findExpensesSum(feedExpensesHamster));
+        sayEnjoyMeal("Байт");
     }
+    public static void sayHello(String name) {
 
-    public static void welcomeUserByName() { // Объявите метод welcomeUserByName
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Как вас зовут?");
-        String name = scanner.nextLine();// Сохраните введённое пользователем имя в переменную name
-        System.out.println("Рад познакомиться, " + name + "!");
+        System.out.println("Привет, " + name +"!");
     }
-
-    public static void sayHelloByTime() {// Объявите метод sayHelloByTime
-
-        Scanner scanner = new Scanner(System.in);
-        int currentHour = scanner.nextInt(); // Спросите у пользователя "Который час?" и сохраните ответ в переменную currentHour
-        if (currentHour < 6) {
-            System.out.println("Доброй ночи!");
-        }// В зависимости от времени предусмотрите печать приветствий
-        else if (currentHour < 12) {
-            System.out.println("Доброе утро!");
-        } else if (currentHour < 18) {
-            System.out.println("Добрый день!");
-        } else if (currentHour >= 22) {
-            System.out.println("Добрый вечер!");
-        } else {
-            System.out.println("Доброй ночи!");
-
+    public static double findMaxExpense(double[] expenses) {
+        double maxFeedExpense = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxFeedExpense) {
+                maxFeedExpense = expenses[i];
+            }
         }
-    }
+        return maxFeedExpense;
 
-    public static void printSuccess() {
-        System.out.println("У вас уже неплохо получается программировать!");
+
     }
-             // Объявите метод printSuccess
-             // Он должен печатать строку: У вас уже неплохо получается программировать!
+    public static double findExpensesSum(double[] expenses) {
+        double sumFeed = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            sumFeed = sumFeed + expenses[i];
+        }
+        return sumFeed;
+
+
+    }
+    public static void sayEnjoyMeal(String name) {
+        System.out.println("Приятного аппетита, " + name);
+
+    }
 }
